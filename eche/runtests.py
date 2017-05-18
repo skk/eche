b9cbcfc88b86c2bce7d699a5c9b739b0c953e9f1
Steven Knight <steven.knight@cashstar.com>
2017-05-18 12:11:23 -0400

    Run tests for step1.

--- eche/runtests.py
+++ eche/runtests.py
@@ -1,12 +1,17 @@
 
 import pathlib
+import traceback
+
+import sys
 
 import eche.step0_repl as step0_repl
-
+import eche.step1_read_print as step1_read_print
+import eche.reader
 
 tests_dir = pathlib.Path('eche').joinpath('tests')
 tests = {
-    'step0_repl.mal': step0_repl
+    'step0_repl.mal': step0_repl,
+    'step1_read_print.mal': step1_read_print
 }
 
 for filename, mod in tests.items():
@@ -14,4 +19,14 @@ for filename, mod in tests.items():
         lines = inbuf.read().split('\n')
         for line in lines:
             print(f"{line}")
-            mod.REP(line)
+            try:
+                mod.REP(line)
+            except eche.reader.Blank:
+                continue
+            except SyntaxError as e:
+                print("".join(traceback.format_exception(*sys.exc_info())))
+                continue
+            except IOError as e:
+                print("".join(traceback.format_exception(*sys.exc_info())))
+                break
+
