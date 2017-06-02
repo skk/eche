82c290001f34e2712e312a4c90c55f9c27cd435f
Steven Knight <steven.knight@cashstar.com>
2017-06-01 20:24:29 -0400

    Add more tests.

--- eche/tests/test_step1_raed_print.py
+++ eche/tests/test_step1_raed_print.py
@@ -3,12 +3,37 @@ import pytest
 from eche.reader import read_str
 from eche.printer import print_str
 
+import math
+
 
 @pytest.mark.parametrize("test_input", [
     '1',
     '-1',
+    '0',
+    str(math.pi),
+    str(math.e)
 ])
-def test_read_atom(test_input):
+def test_numbers(test_input):
+    assert print_str(read_str(test_input)) == test_input
+
+
+@pytest.mark.parametrize("test_input", [
+    '*',
+    '+',
+    'abc',
+    'test1',
+    'abc-def',
+])
+def test_eche_type_symbol(test_input):
+    assert print_str(read_str(test_input)) == test_input
+
+
+@pytest.mark.parametrize("test_input", [
+    '()',
+    '(* 1 2)',
+    '(+ (* 1 5) (/ 1 0))'
+])
+def test_eche_type_list(test_input):
     assert print_str(read_str(test_input)) == test_input
 
 
@@ -23,23 +48,7 @@ def test_nil(test_input):
     'true',
     'false',
 ])
-def test_nil(test_input):
+def test_bool(test_input):
     assert print_str(read_str(test_input)) == test_input
 
 
-@pytest.mark.parametrize("test_input", [
-    '*',
-    'abc',
-    'abc5',
-    'abc-def',
-])
-def test_eche_type_symbol(test_input):
-    assert print_str(read_str(test_input)) == test_input
-
-
-@pytest.mark.parametrize("test_input", [
-    '()',
-    '(* 1 2)',
-])
-def test_eche_type_list(test_input):
-    assert print_str(read_str(test_input)) == test_input
