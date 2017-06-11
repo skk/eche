e5e5b066f617d6a327966c0ea657b6d90fb634d5
Steven Knight <steven.knight@cashstar.com>
2017-06-11 18:25:50 -0400

    Add tests for whitespace, keywords and dicts.

--- eche/tests/test_step1_raed_print.py
+++ eche/tests/test_step1_raed_print.py
@@ -35,7 +35,8 @@ def test_eche_type_symbol(test_input):
     '(+ (* 1 5) (/ 1 0))'
 ])
 def test_eche_type_list(test_input):
-    assert print_str(read_str(test_input)) == test_input
+    a = read_str(test_input)
+    assert print_str(a) == test_input
 
 
 @pytest.mark.parametrize("test_input", [
@@ -53,3 +54,36 @@ def test_bool(test_input):
     assert print_str(read_str(test_input)) == test_input
 
 
+@pytest.mark.parametrize("test_input", [
+    '(1 2, 3,,,,,),,,'
+])
+def test_whitespace(test_input):
+    a = read_str(test_input)
+    b = print_str(a)
+    assert str(a) == b
+
+
+@pytest.mark.parametrize("test_input", [
+    ':kw',
+    '[:kw1 :kw2 :kw3]',
+])
+def test_keywords(test_input):
+    assert print_str(read_str(test_input)) == test_input
+
+
+@pytest.mark.parametrize("test_input", [
+    '[0 1 2 3]',
+])
+def test_vector(test_input):
+    actual = print_str(read_str(test_input))
+    excepted = test_input
+    assert actual == excepted
+
+
+@pytest.mark.parametrize("test_input", [
+    '{"abc" 1}',
+])
+def test_dicts(test_input):
+    a = read_str(test_input)
+    b = test_input
+    assert print_str(a) == b
