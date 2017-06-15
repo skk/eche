7530ef401bc0b136764d1b6e90722d979ca78924
Steven Knight <steven.knight@cashstar.com>
2017-06-15 13:02:14 -0400

    Add functions for Nodes and clean-up code in eval_ast.

--- eche/eval.py
+++ eche/eval.py
@@ -1,14 +1,37 @@
-import operator
-
 from eche.eche_types import List, Symbol, Node
 
+
+def multiply(a: Node, b: Node) -> Node:
+    return Node(data=a.data * b.data)
+
+
+def add(a: Node, b: Node) -> Node:
+    return Node(data=a.data + b.data)
+
+
+def subtract(a: Node, b: Node) -> Node:
+    return Node(data=a.data - b.data)
+
+
+def divide(a: Node, b: Node) -> Node:
+    return Node(data=a.data / b.data)
+
+
+def exp(a: Node, b: Node) -> Node:
+    return Node(data=pow(a.data, b.data))
+
+
+def mod(a: Node, b: Node) -> Node:
+    return Node(data=a.data % b.data)
+
+
 repl_env = {
-    Symbol('+'): operator.add,
-    Symbol('-'): operator.sub,
-    Symbol('*'): operator.mul,
-    Symbol('/'): operator.truediv,
-    Symbol('^'): operator.pow,
-    Symbol('%'): operator.mod
+    Symbol('+'): add,
+    Symbol('-'): subtract,
+    Symbol('*'): multiply,
+    Symbol('/'): divide,
+    Symbol('^'): exp,
+    Symbol('%'): mod
 }
 
 
@@ -23,20 +46,17 @@ def eval_ast(ast, env):
         if len(ast) == 0:
             return ast
 
-        new_list = List()
-        for idx, node in enumerate(ast):
-            val = eval_ast(node, env)
-            new_list.prepend(val)
-
+        ast = [Node(data=eval_ast(node, env)) for node in ast]
         try:
-            fn = new_list[0].data
-        except IndexError:
-            return new_list
+            fn = ast[0].data
+        except (IndexError, AttributeError):
+            fn = None
+
+        if callable(fn):
+            vals = fn(*ast[1:])
+            return vals
         else:
-            if callable(fn):
-                data = [d.data for d in new_list[1:]]
-                val = fn.__call__(*data)
-                return val
+            return ast
     elif isinstance(ast, Symbol):
         return env[ast]
     else:
