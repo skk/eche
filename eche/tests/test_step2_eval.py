30a578bddea4a94be0e4cf01bac8a42133bcdce1
Steven Knight <steven.knight@cashstar.com>
2017-06-16 18:58:50 -0400

    Correct types.

--- eche/tests/test_step2_eval.py
+++ eche/tests/test_step2_eval.py
@@ -3,6 +3,7 @@ import pytest
 
 from eche.reader import read_str
 from eche.eval import eval_ast, repl_env
+from eche_types import Node
 
 
 @pytest.mark.parametrize("test_input,expected_value", [
@@ -12,7 +13,7 @@ from eche.eval import eval_ast, repl_env
 ])
 def test_eval_add(test_input, expected_value):
     ast = read_str(test_input)
-    assert eval_ast(ast, repl_env) == expected_value
+    assert eval_ast(ast, repl_env) == Node(data=expected_value)
 
 
 @pytest.mark.parametrize("test_input,expected_value", [
@@ -21,17 +22,17 @@ def test_eval_add(test_input, expected_value):
 ])
 def test_eval_sub(test_input, expected_value):
     ast = read_str(test_input)
-    assert eval_ast(ast, repl_env) == expected_value
+    assert eval_ast(ast, repl_env) == Node(data=expected_value)
 
 
 @pytest.mark.parametrize("test_input,expected_value", [
     ('(* 50 60)', 3_000),
-    ('(* 2 3)', 8),
+    ('(* 2 3)', 6),
     ('(+ 2 (* 3 4))', 14),
 ])
 def test_eval_mul(test_input, expected_value):
     ast = read_str(test_input)
-    assert eval_ast(ast, repl_env) == expected_value
+    assert eval_ast(ast, repl_env) == Node(data=expected_value)
 
 
 @pytest.mark.parametrize("test_input,expected_value", [
@@ -39,17 +40,18 @@ def test_eval_mul(test_input, expected_value):
 ])
 def test_eval_div(test_input, expected_value):
     ast = read_str(test_input)
-    assert eval_ast(ast, repl_env) == expected_value
+    assert eval_ast(ast, repl_env) == Node(data=expected_value)
 
 
 @pytest.mark.parametrize("test_input,expected_value", [
-    ('(^ 1 2)', 0.5),
-    ('(^ 3 4)', 0.75),
-    ('(^ 3 4)', 0.75),
+    ('(^ 2 2)', 8),
+    ('(^ 3 4)', 81),
+    ('(^ 10^10)', 10_000_000_000),
 ])
 def test_eval_exp(test_input, expected_value):
     ast = read_str(test_input)
-    assert eval_ast(ast, repl_env) == expected_value
+    assert eval_ast(ast, repl_env) == Node(data=expected_value)
+
 
 @pytest.mark.parametrize("test_input,expected_value", [
     ('(% 8 5)', 3),
@@ -57,4 +59,4 @@ def test_eval_exp(test_input, expected_value):
 ])
 def test_eval_exp(test_input, expected_value):
     ast = read_str(test_input)
-    assert eval_ast(ast, repl_env) == expected_value
+    assert eval_ast(ast, repl_env) == Node(data=expected_value)
